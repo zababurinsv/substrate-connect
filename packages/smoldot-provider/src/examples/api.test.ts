61eb1962b5fc1416b15eb67692c7627ed3e2061a
Raoul Millais <raoul@raoulmillais.com>
2021-02-12 13:49:06 +0000

    Migrate tests

--- packages/smoldot-provider/src/examples/api.test.ts
+++ packages/smoldot-provider/src/examples/api.test.ts
@@ -1,7 +1,6 @@
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { readFile } from 'fs/promises';
-import anyTest, {TestInterface} from 'ava';
 import { ApiPromise } from '@polkadot/api';
 import {SmoldotProvider} from '../';
 import {logger} from '@polkadot/util';
@@ -13,63 +12,65 @@ const __dirname = dirname(__filename);
 
 const l = logger('examples');
 
-const test = anyTest as TestInterface<{api: ApiPromise}>;
+let api: ApiPromise | undefined = undefined;
 
-test.before('Create a smoldot client', async t => {
+
+beforeAll(async () => {
   const chainSpec = await readFile(join(__dirname, 'westend.json'), 'utf-8');
   const database = new FsDatabase('test');
   const provider = new SmoldotProvider(chainSpec, database);
   await provider.connect();
-  t.context.api = await ApiPromise.create({ provider });
+  api = await ApiPromise.create({ provider });
   l.log('API is ready');
-  t.truthy(t.context.api);
+  expect(api);
 });
 
-test('API constants', async t => {
-  const api = t.context.api;
+test('API constants', async () => {
+  if (!api) { throw new Error('api was not initialised by before hook'); }
   const genesisHash = api.genesisHash.toHex();
   l.log('genesis hash: ', genesisHash);
-  t.not(genesisHash, '');
+  expect(genesisHash).not.toBe('');
   const epochDuration = api.consts.babe.epochDuration.toNumber();
   l.log('epoch duration: ', epochDuration);
-  t.true(epochDuration > 0);
+  expect(epochDuration > 0).toBe(true);
   const existentialDeposit = api.consts.balances.existentialDeposit.toHuman();
   l.log('existentialDeposit' , existentialDeposit);
-  t.not(genesisHash, '');
+  expect(genesisHash).not.toBe('');
 });
 
 // This errors and error handling isnt yet implemented for storage queries
 // in smoldot: https://github.com/paritytech/smoldot/issues/388
 test.skip('State queries', async t => {
-  const api = t.context.api;
+  if (!api) { throw new Error('api was not initialised by before hook'); }
   const testAddress = '5FHyraDcRvSYCoSrhe8LiBLdKmuL9ptZ5tEtAtqfKfeHxA4y';
   const now = await api.query.timestamp.now();
   const { nonce, data: balance } = await api.query.system.account(testAddress);
   l.log(`balance of ${balance.free} and a nonce of ${nonce}`);
-  t.pass();
+  expect(true);
 });
 
-test('RPC queries', async t => {
-  const api = t.context.api;
+test('RPC queries', async () => {
+  if (!api) { throw new Error('api was not initialised by before hook'); }
   const chain = await api.rpc.system.chain();
   const lastHeader = await api.rpc.chain.getHeader();
   l.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
-  t.pass();
+  expect(true);
 });
 
-test('RPC query subscriptions', async t=> {
-  const api = t.context.api;
+test('RPC query subscriptions', async () => {
+  if (!api) { throw new Error('api was not initialised by before hook'); }
   const chain = await api.rpc.system.chain();
 
   return new Promise<void>((resolve, reject) => {
+    if (!api) { throw new Error('api was not initialised by before hook'); }
     let unsubscribe: any = undefined;
-    return api.rpc.chain.subscribeNewHeads((lastHeader) => {
+    return api.rpc.chain.subscribeNewHeads((lastHeader: any) => {
       l.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
       unsubscribe();
-      t.pass();
+      expect(true);
       resolve();
     }).then(cb => {
-        unsubscribe = cb;
+      unsubscribe = cb;
     });
   });
 });
---
+++ projects/smoldot-extension/src/AppMediator.test.ts
@@ -0,0 +1,8 @@
+import test from 'ava';
+import { spy } from 'sinon';
+import chrome from 'sinon-chrome';
+import { AppMediator } from './AppMediator';
+
+test('ava works', t => {
+  t.pass('woohoo!');
+});
